c5dc64083b4a19dd86b79e040863ac655eaae0f9
Alexsander Gomes <alexsandergomes@Alexsanders-MacBook-Pro.local>
2024-03-09 13:33:59 -0800

    default header and directory page created

--- buffer/disk-manager.go
+++ buffer/disk-manager.go
@@ -29,6 +29,11 @@ func NewDiskManager(filename string, headerSize int64) (*DiskManager, error) {
 		HeaderSize: headerSize,
 	}
 
+	err = dm.SetDefaultHeader()
+	if err != nil {
+		return nil, err
+	}
+
 	if err := dm.loadOrCreateDirectoryPage(); err != nil {
 		return nil, err
 	}
@@ -37,7 +42,7 @@ func NewDiskManager(filename string, headerSize int64) (*DiskManager, error) {
 }
 
 func (dm *DiskManager) loadOrCreateDirectoryPage() error {
-	directoryPageOffset, err := dm.readDirectoryPageOffset()
+	directoryPageOffset, err := dm.readHeader()
 	if err != nil {
 		return err
 	}
@@ -55,6 +60,80 @@ func (dm *DiskManager) loadOrCreateDirectoryPage() error {
 	return nil
 }
 
+func (dm *DiskManager) createDirectoryPage() error {
+	dm.DirectoryPage = DirectoryPage{
+		Mapping: make(map[PageID]Offset),
+	}
+
+	dirPageBytes, err := encodeDirectoryPage(dm.DirectoryPage)
+	if err != nil {
+		return err
+	}
+
+	pageLocation, err := dm.writePageToFile(dirPageBytes)
+	if err != nil {
+		return err
+	}
+
+	if err := dm.updateHeader(pageLocation); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (dm *DiskManager) readHeader() (Offset, error) {
+	headerBytes := make([]byte, dm.HeaderSize)
+	_, err := dm.File.ReadAt(headerBytes, 0)
+	if err != nil {
+		return 0, err
+	}
+
+	isDefaultHeader := true
+	for _, b := range headerBytes {
+		if b != 0 {
+			isDefaultHeader = false
+			break
+		}
+	}
+
+	if isDefaultHeader {
+		return 0, nil
+	}
+
+	offsetBytes := headerBytes[:8]
+	offset := Offset(binary.BigEndian.Uint64(offsetBytes))
+
+	return offset, nil
+}
+
+func (dm *DiskManager) SetDefaultHeader() error {
+	fileInfo, err := dm.File.Stat()
+	if err != nil {
+		return err
+	}
+	fileSize := fileInfo.Size()
+
+	if fileSize < dm.HeaderSize {
+		if err := dm.initializeHeader(); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (dm *DiskManager) initializeHeader() error {
+	defaultHeader := make([]byte, dm.HeaderSize)
+
+	_, err := dm.File.Write(defaultHeader)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func (dm *DiskManager) LoadDirectoryPage(offset Offset) error {
 	_, err := dm.File.Seek(int64(offset), io.SeekStart)
 	if err != nil {
@@ -67,7 +146,6 @@ func (dm *DiskManager) LoadDirectoryPage(offset Offset) error {
 		return err
 	}
 
-	// Decode the directory page bytes into the directoryPage field of DiskManager
 	err = json.Unmarshal(dirPageBytes, &dm.DirectoryPage)
 	if err != nil {
 		return err
@@ -76,54 +154,17 @@ func (dm *DiskManager) LoadDirectoryPage(offset Offset) error {
 	return nil
 }
 
-func (dm *DiskManager) readDirectoryPageOffset() (Offset, error) {
-	headerBytes := make([]byte, dm.HeaderSize)
-	if _, err := dm.File.ReadAt(headerBytes, 0); err != nil {
-		return 0, err
-	}
-
-	offsetBytes := headerBytes[:8]
-
-	offset := Offset(binary.BigEndian.Uint64(offsetBytes))
-	return offset, nil
-}
-
-func (dm *DiskManager) createDirectoryPage() error {
-	dm.DirectoryPage = DirectoryPage{
-		Mapping: make(map[PageID]Offset),
-	}
-
-	dirPageBytes, err := encodeDirectoryPage(dm.DirectoryPage)
-	if err != nil {
-		return err
-	}
-
-	pageLocation, err := dm.writePageToFile(dirPageBytes);
-	if err != nil {
-		return err
-	}
-
-	if err := dm.updateHeader(pageLocation); err != nil {
-		return err
-	}
-
-	if err := dm.LoadDirectoryPage(pageLocation); err != nil {
-		return err
-	}
-
-	return nil
-}
-
+// TASK : debug this
 func (dm *DiskManager) updateHeader(offset Offset) error {
-	headerBytes := make([]byte, dm.HeaderSize)
-	if _, err := dm.File.ReadAt(headerBytes, 0); err != nil {
-		return err
-	}
-
 	offsetBytes := make([]byte, 8)
 	binary.BigEndian.PutUint64(offsetBytes, uint64(offset))
 
-	copy(headerBytes, offsetBytes)
+	headerBytes := make([]byte, dm.HeaderSize)
+	if _, err := dm.File.ReadAt(headerBytes, 0); err != nil {
+		return err
+	}
+
+	copy(headerBytes[dm.HeaderSize:], offsetBytes)
 
 	_, err := dm.File.WriteAt(headerBytes, 0)
 	if err != nil {
@@ -134,7 +175,7 @@ func (dm *DiskManager) updateHeader(offset Offset) error {
 }
 
 func (dm *DiskManager) writePageToFile(pageBytes []byte) (Offset, error) {
-	offset := dm.HeaderSize + 1
+	offset := dm.HeaderSize
 	_, err := dm.File.WriteAt(pageBytes, offset)
 	if err != nil {
 		return 0, err
--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"disk-db/buffer"
+	"fmt"
+	"log"
+)
+
+const HeaderSize = 8
 func main() {
-	
+	diskPtr, err := buffer.NewDiskManager("database1", HeaderSize)
+	if err != nil {
+		log.Println(err)
+	}
+
+	fmt.Println(diskPtr.DirectoryPage)
 }
